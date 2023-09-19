52ae2d37c5f2ed6399f4c07dc63f9cba7af8591b
zacharyGayford <zachary.gayford@gmail.com>
2023-09-19 13:13:19 -0500

    added sources

--- src/parser.h
+++ src/parser.h
@@ -1,12 +1,17 @@
 #include <libxml++/libxml++.h>
 
+// https://developer-old.gnome.org/libxml++-tutorial/stable/ch02s02.html
+// https://www.microfocus.com/documentation/visual-cobol/vc60/DevHub/GUID-8BC34B95-A81A-4E36-B763-07FB24982DEE.html
+
 class ParserXML : public xmlpp::SaxParser {
 	
 	public:
+
 		ParserXML ();
 		~ParserXML ();
 
 	protected:
+
 		void on_start_document () override;
 		void on_end_document () override;
 		void on_start_element (const xmlpp::ustring& name, const AttributeList& properties) override;
